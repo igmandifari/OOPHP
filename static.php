a23b43ba294be2e16afe7c15613873760c5e44bc
Igman Difari <54891762+igmandifari@users.noreply.github.com>
2019-10-12 17:23:10 +0700

    add

---
+++ static.php
@@ -0,0 +1,18 @@
+<?php 
+
+class ContohStatic{
+    public static $angka = 1;
+
+    public static function halo(){
+        return "halo." . self::$angka++ . "kali.";
+    }
+
+}
+
+echo ContohStatic::$angka;
+echo "<br>";
+echo ContohStatic::halo();
+echo "<hr>";
+echo ContohStatic::halo();
+
+?>
